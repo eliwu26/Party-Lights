c5aa7e02aed05fbfe5fc31eec32176766455a603
Charles Lu <charleslu@live.com>
2016-03-29 00:47:50 -0700

    Update Charles config

--- AudioLights2/config_charles.h
+++ AudioLights2/config_charles.h
@@ -2,11 +2,12 @@
 #define MODE_PARTY 1
 #define MODE_SLOW 2
 #define MODE_GREENPULSE 3
-#define MODE_BASSFADE 4
+
+#define NUM_MODES 4
 
 #define MODE_MODE_WHITEPULSE -1
 #define MODE_GREEN -1
 #define MODE_RED -1
 #define MODE_WHITE -1
+#define MODE_BASSFADE -1
 
-#define NUM_MODES 5
